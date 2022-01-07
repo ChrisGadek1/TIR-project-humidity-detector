a5413530e4abe2817f0842ae7db566fc39b8f29e
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-01-07 15:59:35 +0100

    fixed rain and sprinkling information delivery

--- detectors/czujnik.py
+++ detectors/czujnik.py
@@ -5,12 +5,14 @@ import sys
 import mosquitto
 
 humidity = int(sys.argv[1])
-rain_or_sprinkling = False if sys.argv[2] == "False" else True
+rain = False if sys.argv[2] == "False" else True
 time_to_dry = float(sys.argv[3])
 mqqtc_server = sys.argv[4]
 id = int(sys.argv[5])
 sector_id = None
 desired_humidity = None
+
+working_sprinklers = []
 
 mqttc = mosquitto.Mosquitto()
 
@@ -18,13 +20,13 @@ def on_connect(mqttc, obj, rc):
     print("rc: "+str(rc))
 
 def on_message(mqttc, obj, msg):
-    global sector_id, desired_humidity, rain_or_sprinkling
+    global sector_id, desired_humidity, rain
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
     if msg.topic == "agh/iot/project9/config":
         try:
             msg_dict = json.loads(msg.payload)
             for sector in msg_dict["sectors"]:
-                if sector["sensor_id"] == id:
+                if sector["sensor_id"] == str(id):
                     sector_id = int(sector["id"])
                     desired_humidity = int(sector["desired_humidity"])
                     mqttc.subscribe("agh/iot/project9/simulation/area/"+str(sector_id)+"/rain", 2)
@@ -35,10 +37,18 @@ def on_message(mqttc, obj, msg):
     elif msg.topic == "agh/iot/project9/sensor/request":
         mqttc.publish("agh/iot/project9/sensor/"+str(id)+"/humidity", str(humidity), 2, False)
     elif msg.topic == "agh/iot/project9/simulation/area/"+str(sector_id)+"/rain":
-        if msg.payload == b"water":
-            rain_or_sprinkling = True
-        else:
-            rain_or_sprinkling = False
+        try:
+            sprinkling_data = json.loads(msg.payload)
+            if sprinkling_data["rain"] != "None":
+                rain = True if sprinkling_data["rain"].lower() == "true" else False
+            else:
+                sprinkler_id = int(sprinkling_data["sprinkler_id"])
+                if str(sprinkling_data["is_watering"]).lower() == "true":
+                    working_sprinklers.append(sprinkler_id)
+                else:
+                    working_sprinklers.remove(sprinkler_id)
+        except Exception as e:
+            print("json with incorrect format, " + str(e))
 
 
 
@@ -53,14 +63,19 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 def humidity_thread():
-    global humidity, rain_or_sprinkling
+    global humidity, rain
     while True:
-        if humidity > 0 and not rain_or_sprinkling:
+        if humidity > 0 and not rain and len(working_sprinklers) == 0:
             humidity -= 1
-        elif humidity < 100 and rain_or_sprinkling:
-            humidity = humidity + 5 if humidity + 5 < 100 else 100
+        if humidity < 100 and rain:
+            humidity = humidity + 3 if humidity + 3 < 100 else 100
+        if len(working_sprinklers) > 0 and humidity < 100:
+            humidity = humidity + 2*len(working_sprinklers) if humidity + 2*len(working_sprinklers) < 100 else 100
         print("current humidity: "+str(humidity))
-        print("is raining or sprinkling: "+str(rain_or_sprinkling))
+        print("is raining: "+str(rain))
+        print("number of working sprinklers: "+str(len(working_sprinklers)))
+        print("id: "+str(id))
+        print("sector_id: "+str(sector_id))
         print("========================")
         time.sleep(time_to_dry)
 
